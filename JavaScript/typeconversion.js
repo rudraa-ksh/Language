89f4165c11597f99738c4366b2e58e3f24695465
Rudra18GTHB <rudrak04sh@gmail.com>
2023-11-18 22:07:16 +0530

    Data Type Conversion

---
+++ JavaScript/typeconversion.js
@@ -0,0 +1,10 @@
+let score = "33"
+console.log(typeof score);
+console.log(typeof Number(score));
+console.log(typeof score);
+
+let value = "33abc"
+console.log(typeof value);
+let valueinnumber= Number(value)
+console.log(typeof valueinnumber);
+console.log(valueinnumber); // true results 1, null results 0 and undefined results NaN

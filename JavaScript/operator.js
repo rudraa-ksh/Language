09c955b7e77ded58fc4c7aeba25ccb2e69447acc
Rudra18GTHB <rudrak04sh@gmail.com>
2023-11-21 22:13:32 +0530

    Operators

---
+++ JavaScript/operator.js
@@ -0,0 +1,8 @@
+console.log("2"> 0); // converts it into 2.
+console.log(null > 0); //here >= or <= treats differently they convert null to 0. 
+console.log(null== 0);
+console.log(null>= 0);
+console.log(undefined == 0); //when undefined always false.
+console.log(undefined >= 0);
+console.log(undefined > 0);
+console.log("2" === 2); //strict check returns false

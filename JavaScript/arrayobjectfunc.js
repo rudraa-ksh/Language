7eb80e21db6d2cecac5064464cde5ab178154e5e
Rudra18GTHB <rudrak04sh@gmail.com>
2023-11-22 22:57:34 +0530

    Array,object and Function

---
+++ JavaScript/arrayobjectfunc.js
@@ -0,0 +1,11 @@
+const names =["Rudra","Abdur","Om"]
+console.log(names);
+let myobj={
+    name:"Rudra",
+    age: 19,
+}
+console.log(myobj);
+const myfunc = function(){
+    console.log("My name is Rudra");
+}
+myfunc()

0fb0bc30d0ba1a62d9e22387517a97a254853954
Rudra18GTHB <rudrak04sh@gmail.com>
2023-12-04 19:48:15 +0530

    Implemented: JDBC

---
+++ Java/SQL/src/App.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+
+public class App{
+    public static void main(String[] args) {
+        try{
+            String url="jdbc:mysql://localhost:3306/opju";
+            String username="root";
+            String password="rudraxcode";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url,username,password);
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("Select * from sales");
+        
+            while(rs.next()){
+                System.out.println(rs.getString(2));
+            }
+            con.close();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}

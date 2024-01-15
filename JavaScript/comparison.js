1252961f76e80a56e0c3c0215cc68e649cfbc7c5
Rudra18GTHB <rudrak04sh@gmail.com>
2024-01-15 14:04:58 +0530

    Learned : Null with operators

---
+++ JavaScript/comparison.js
@@ -0,0 +1,7 @@
+console.log("2">1); //true
+console.log("02" > 1); //true
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+// === is strict check it will not convert automatically
+//comparison operators convert null to 0 

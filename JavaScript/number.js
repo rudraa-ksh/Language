50237bfa44ddd672d2f55c2fb8c70228a9da72ff
rudraa-ksh <rudrak04sh@gmail.com>
2024-02-26 20:34:43 +0530

    Learned: Number

---
+++ JavaScript/number.js
@@ -0,0 +1,8 @@
+const balance = new Number(4000)
+let account =100;
+console.log(account);
+console.log(balance);
+console.log(balance.toFixed(1));
+let num = 123.5899
+console.log(num.toPrecision(8));
+console.log(balance.toLocaleString('en-IN'));

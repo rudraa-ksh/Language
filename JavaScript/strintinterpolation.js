ffbe60d773ad419f0a9a60c3f43df32b92e1e268
rudraa-ksh <rudrak04sh@gmail.com>
2024-02-26 20:14:39 +0530

    Learned: String

---
+++ JavaScript/strintinterpolation.js
@@ -0,0 +1,18 @@
+let firtsname ="Rudra"
+let lastname ="Kashyap"
+
+
+console.log(`My first name is ${firtsname} and my last name is ${lastname}`);
+console.log(firtsname.length);
+console.log(firtsname.toUpperCase());
+console.log(firtsname.toLowerCase());
+console.log(firtsname.charAt(2));
+console.log(firtsname.indexOf('u'));
+console.log(firtsname.substring(0,4));  //can give negative value but it will start from 0
+console.log(firtsname.slice(-5,5));
+let email = "  rudra@gmail.com   "
+console.log(email);
+console.log(email.trim());
+console.log(email.replace("@","$"));
+console.log(email.includes("gmail.com"));
+console.log(email.split("@"));

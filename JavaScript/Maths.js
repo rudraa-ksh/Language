59673c229c0ed80272f59752d891b5341cebe2d2
rudraa-ksh <rudrak04sh@gmail.com>
2024-02-28 10:56:19 +0530

    Learned: Math

---
+++ JavaScript/Maths.js
@@ -0,0 +1,8 @@
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.1));
+console.log(Math.min(1,2,3,4,54));
+console.log(Math.max(1,2,3,4,54));
+console.log(Math.random());  // return value between 0 and 1
+console.log(Math.random()*10); // return value between 1 to 10

c3c0742b519cb08f64eb0d3a8bf75f4a9d02befc
rudraa-ksh <rudrak04sh@gmail.com>
2024-04-08 10:24:11 +0530

    Started: Function

---
+++ JavaScript/Functions.js
@@ -0,0 +1,28 @@
+function SayMyName(){
+    console.log("Rudra");
+    console.log("Kashyap");
+}
+
+SayMyName()
+SayMyName()
+SayMyName()
+SayMyName()
+
+function Add2Num(num1, num2){
+    console.log(num1+num2)
+}
+
+Add2Num(1,2)
+
+function AddTwoNum(num1, num2){
+    return num1+num2
+}
+
+Add2Num(1,2)
+
+
+function Login(username){
+    return `${username} logged in`
+}
+
+console.log(Login("Rudra Kashyap"))

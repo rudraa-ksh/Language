8537e7315db24670596fdf4ca220b5d2a04315d4
rudraa-ksh <rudrak04sh@gmail.com>
2024-04-14 11:09:44 +0530

    Loops in JavaScript

---
+++ JavaScript/Iterations.js
@@ -0,0 +1,18 @@
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element);
+    
+}
+
+let myArray = ["Ironman", "Batman", "Spiderman"]
+let i = 0;
+while (i<myArray.length){
+    console.log(`Character ${myArray[i]}`);
+    i++
+}
+
+let Score = 0
+do {
+    console.log(`Score is ${Score}`);
+    Score++
+} while (Score<10);

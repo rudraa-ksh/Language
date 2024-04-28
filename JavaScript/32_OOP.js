1f2620cb32dee5d7ead48c99093d4e1504c08c02
rudraa-ksh <rudrak04sh@gmail.com>
2024-04-28 20:32:05 +0530

    object, this, new and constructor

---
+++ JavaScript/32_OOP.js
@@ -0,0 +1,30 @@
+const user = {
+    username: "Rudra",
+    loginCount: 8,
+    signedIn: true,
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+
+}
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+    }
+    return this
+}
+
+const userOne = new User("Rudra", 12, true)
+const userTwo = new User("Scorpio", 11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);

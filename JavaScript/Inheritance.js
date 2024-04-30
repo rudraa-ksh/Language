6b98bad20e843ddc926053d07e66043a9857598f
rudraa-ksh <rudrak04sh@gmail.com>
2024-04-30 20:03:38 +0530

    Class: Inheritance

---
+++ JavaScript/Inheritance.js
@@ -0,0 +1,26 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username is ${this.username}`)
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const rudra = new Teacher("rudraksh","rudra@opju.ac.in","123")
+rudra.addCourse()
+
+console.log(rudra instanceof Teacher);

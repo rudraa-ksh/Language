f5691f708ac4c8ac9a6ca795119aeaba87b5c0aa
rudraa-ksh <rudrak04sh@gmail.com>
2024-05-05 20:09:13 +0530

    TypeScript: Variable and Datatypes

--- TypeScript/Variable.js
+++ TypeScript/Variable.js
@@ -1,2 +1,7 @@
+//String
 var car = "BMW M8";
 console.log(car);
+//export {} remove the error temprarily
+//Number
+var userId = 1235;
+console.log(userId);
--- TypeScript/Variable.ts
+++ TypeScript/Variable.ts
@@ -1,6 +1,17 @@
+
+//String
 const car: string = "BMW M8"
 
 console.log(car);
 
+export {} //remove the error temprarily
 
-//export {} remove the error temprarily
+//Number it is same for int and float
+let userId: number = 1235;  //userid will always be a number.
+console.log(userId);
+
+
+//boolean
+
+let isLogggedIn:boolean = true;
+console.log(isLogggedIn);

da1826b88bf2b3a9a5e08318e221f646c79e52b7
rudraa-ksh <rudrak04sh@gmail.com>
2024-05-11 23:55:46 +0530

    TypeScript: Tuples

---
+++ TypeScript/Tuples.ts
@@ -0,0 +1,11 @@
+let User: [string, number, boolean]
+
+User = ["name", 123, true] // arranged in same order(Compulsory)
+
+let RGB: [number, number, number] = [255,123,123]
+
+type Person = [number, string]
+
+const P1 : Person =[112, "Rahul"]
+
+P1[0] = 101;

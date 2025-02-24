f1184cd9cac27f4e4605767769cf516a40bb3ddc
Rudra Kashyap <113619440+rudraa-ksh@users.noreply.github.com>
2025-02-24 10:44:06 +0000

    Add: Type Casting

---
+++ C++/casting.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a =90.123;
+    cout<< a<<endl;
+
+    float b = 9012562365;
+    cout <<b<<endl;
+    b= 90.12345;
+    cout <<b<<endl;
+
+    short c = (int) b;
+    cout << c << endl;
+
+}
